b56994e0878a0b09e1b94d3f3ede1a1df4b99227
Corey Trombley <corey.trombley33@gmail.com>
2013-02-25 14:09:58 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Avbl::Application.config.secret_token = '06775f2e8d27b84cfe735375447e9ac493425ce25c9fcccd37de662e780e291c55378c7f0c21038c3315a02ad7391048864620252c64d9d4a03c743aa25a8b09'
