f6d01c50c20a360b739bc399d525f317defa91d3
Corey Trombley <corey.trombley33@gmail.com>
2013-03-11 15:35:43 -0400

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+    User.create(first_name:'Chris', last_name:'Trombley', email:'chris@test.com', password:'Skater33')
+    User.create(first_name:'Corey', last_name:'Trombley', email:'corey@test.com', password:'Skater33')
+    User.create(email:'mike@test.com', password:'Skater33')
+    User.create(email:'jim@test.com', password:'Skater33')
+    User.create(email:'mark@test.com', password:'Skater33')
+    User.create(email:'jane@test.com', password:'Skater33')
