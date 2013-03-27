5d3aa24edb35197657c17c98989eb46657c77fe9
Corey Trombley <corey.trombley33@gmail.com>
2013-03-27 01:16:42 -0400

    changed the user layout

--- app/admin/friendships.rb
+++ app/admin/friendships.rb
@@ -1,4 +1,11 @@
 ActiveAdmin.register Friendship do
   menu :priority => 3
   
+  form do |f|                         
+    f.inputs "Friendships" do     
+      f.input :user
+      f.input :friend               
+    end                               
+    f.actions                         
+  end
 end
